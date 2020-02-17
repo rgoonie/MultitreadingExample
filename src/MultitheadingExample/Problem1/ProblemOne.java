799aedc488eb484dfa90083847d1df4691a61d92
rajeev <rajeev.goonie98@gmail.com>
2020-02-17 14:24:52 -0800

    created constuctor and instance var to hold n threads

--- src/MultitheadingExample/Problem1/ProblemOne.java
+++ src/MultitheadingExample/Problem1/ProblemOne.java
@@ -2,8 +2,15 @@ package MultitheadingExample.Problem1;
 
 public class ProblemOne {
 
+    private Thread[] allThreads;
+
     public ProblemOne(int n){
-        
+
+        allThreads = new Thread[n];
+
+        for(int i = 0; i < n; i++)
+            allThreads[i] = new Thread();
+
     }
 
 }
