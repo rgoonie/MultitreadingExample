77e07e9917a8bf1749c537c6d44fd05aaeaa7999
rajeev <rajeev.goonie98@gmail.com>
2020-02-17 21:29:00 -0800

    added code to make the thing run

--- src/MultitheadingExample/Problem3/ProblemThree.java
+++ src/MultitheadingExample/Problem3/ProblemThree.java
@@ -3,7 +3,15 @@ package MultitheadingExample.Problem3;
 public class ProblemThree {
 
     public static void main(String[] args){
-        
+        Consumer theConsumer = new Consumer( null );
+        Producer theProducer = new Producer( 10, theConsumer);
+        theConsumer.setMyProducer(theProducer);
+
+        Thread producer = new Thread(theProducer);
+        Thread consumer = new Thread(theConsumer);
+
+        producer.start();
+        consumer.start();
     }
 
 }
