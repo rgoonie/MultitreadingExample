997a7b00ec9c908d22ebab59ff149e1b02942e90
rajeev <rajeev.goonie98@gmail.com>
2020-02-17 19:04:59 -0800

    add constuctor code

--- src/MultitheadingExample/Problem2/Tunnel.java
+++ src/MultitheadingExample/Problem2/Tunnel.java
@@ -1,4 +1,16 @@
 package MultitheadingExample.Problem2;
 
 public class Tunnel {
+
+    private Person[] people;
+
+    public Tunnel(int n){
+        people = new Person[n];
+
+        for(int i = 0; i < n; i++){
+            people[i] = new Person( getRandomName() );
+        }
+
+    }
+
 }
