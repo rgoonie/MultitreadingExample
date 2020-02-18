2df334356456ae860379f5d4dcef93c42b87ca2a
rajeev <rajeev.goonie98@gmail.com>
2020-02-17 18:55:09 -0800

    added code for Person based off desciption

--- src/MultitheadingExample/Problem2/Person.java
+++ src/MultitheadingExample/Problem2/Person.java
@@ -1,4 +1,21 @@
 package MultitheadingExample.Problem2;
 
-public class Person {
+public class Person extends Thread {
+
+    private String name;
+
+    public Person(String name){
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+
+        try{
+            Thread.sleep(5000);
+            System.out.println(name + " has crossed the tunnel...");
+        }
+        catch(Exception e) { System.out.println("Thread failed..."); }
+
+    }
 }
