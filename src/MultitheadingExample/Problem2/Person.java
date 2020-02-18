228f0d9d96d59c678a83c8d1b69d12d145e752ea
rajeev <rajeev.goonie98@gmail.com>
2020-02-17 18:50:03 -0800

    added project files

---
+++ src/MultitheadingExample/Problem2/Person.java
@@ -0,0 +1,4 @@
+package MultitheadingExample.Problem2;
+
+public class Person {
+}
---
+++ src/MultitheadingExample/Problem2/ProblemTwo.java
@@ -0,0 +1,9 @@
+package MultitheadingExample.Problem2;
+
+public class ProblemTwo {
+
+    public static void main(String[] args){
+
+    }
+
+}
