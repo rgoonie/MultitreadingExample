fe17b08d4c3937f6a080df84f234cf6def05856b
rajeev <rajeev.goonie98@gmail.com>
2020-02-17 21:18:43 -0800

    wrote consumer code

--- src/MultitheadingExample/Problem3/Consumer.java
+++ src/MultitheadingExample/Problem3/Consumer.java
@@ -1,6 +1,28 @@
 package MultitheadingExample.Problem3;
 
+import java.util.Queue;
+
 public class Consumer implements Runnable {
+
+    private Producer myProducer;
+
+    public Consumer(Producer producer){
+        myProducer = producer;
+    }
+
+    public void consume() throws Exception{
+        Queue<Integer> market = myProducer.getProduce();
+
+        synchronized (myProducer){
+            while(market.isEmpty()){
+                this.wait();
+            }
+        }
+
+        market.poll();
+        System.out.println("Consumer consumed something...");
+    }
+
     @Override
     public void run() {
 
